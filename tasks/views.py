8bb198e26bcd7ae2de43f56c15e8acc0497a4c7b
MelFxrd <qdaewsfrftgf123@gmail.com>
2025-05-28 13:35:51 +0300

    Сделал шаблон base.html и index.html

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Привет, это To Do List")
+    return render(request, 'index.html')
