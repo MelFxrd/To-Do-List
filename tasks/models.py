3afa188c4b8c7c081c429ca790cf3cb40aceea24
MelFxrd <qdaewsfrftgf123@gmail.com>
2025-05-28 14:42:36 +0300

    Добавил модель задач

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateField(null=True, blank=True)
+    is_completed = models.BooleanField(default=False)
+    priority = models.IntegerField(default=1)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
