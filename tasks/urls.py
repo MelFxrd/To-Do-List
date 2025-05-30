e4198482af7b948e96263e4461f6737a3a964589
MelFxrd <qdaewsfrftgf123@gmail.com>
2025-05-30 19:49:31 +0300

    Добавил регистрацию и авторизацию

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import index
+from .views import task_list
 
 urlpatterns = [
-    path('', index, name='index'),
+    path("", task_list, name="task_list")
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Task
 
-def index(request):
-    return render(request, 'index.html')
+def task_list(request):
+    tasks = Task.objects.all().order_by('deadline', 'priority')
+    return render(request, "tasks/task_list.html", {"tasks": tasks})
