b67ba899b0b97d0d98c9dab9312c2ef5af5ad0d3
MelFxrd <qdaewsfrftgf123@gmail.com>
2025-06-06 10:54:36 +0300

    Оптимизировал структуру и подправил код

--- tasks/views.py
+++ tasks/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from .models import Task
 
 def task_list(request):
-    tasks = Task.objects.all().order_by('deadline', 'priority')
-    return render(request, "tasks/task_list.html", {"tasks": tasks})
+    tasks = Task.objects.all()
+    context = {
+        'tasks': tasks,
+    }
+    return render(request, 'task_list.html', context)
