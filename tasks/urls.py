aa79bd8edda12e1220d604835a8dd6bde68ce2d7
MelFxrd <qdaewsfrftgf123@gmail.com>
2025-06-19 11:05:10 +0300

    Сделал кнопку выхода в списке задач + для неё css

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import task_list, edit_task
+from django.contrib.auth.views import LogoutView
+
 
 urlpatterns = [
     path("", task_list, name="task_list"),
     path("edit/<int:task_id>/", edit_task, name="edit_task"),
+    path("logout/", LogoutView.as_view(next_page="/"), name="logout"),
 ]
